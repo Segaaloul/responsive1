20b0e0b7bacad165cdbc314a61236da6051a7784
Segaaloul <selim03gaaloul@gmail.com>
2022-08-10 17:59:07 +0200

    smart

---
+++ index.js
@@ -0,0 +1,8 @@
+const mediaQuery = window.matchMedia('(max-width: 480px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // Then trigger an alert
+  
+}else{
+    alert("please use a smartphone")
+}
